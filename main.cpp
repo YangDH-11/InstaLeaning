6638cd3797a0a79fcf129fe2428a47ae1690ec85
YangDH-11 <3218526163@qq.com>
2023-07-30 00:48:02 +0800

    <feat>(main): add fun() in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 
 using namespace std;
 
+void fun(void){
+  cout << "run fun" << endl;
+}
 
 int main(int argc, char *argv[]) {
   
   cout << "Hello Word!" << endl;
   cout << "Hello Insta360!" << endl;
-
+  fun();
+  
   return 0;
 }
